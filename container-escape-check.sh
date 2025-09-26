5d1b74e3eca40bd3123a81cce6e7e44819de4678
小墨(wabzsy) <wabzsy@hotmail.com>
2025-09-26 14:07:33 +0800

    add: k8s support

--- container-escape-check.sh
+++ container-escape-check.sh
@@ -104,7 +104,7 @@ CheckTheCurrentEnvironment(){
     if [ ! -f "/proc/1/cgroup" ];then
         IsContainer=0
     else
-        cat /proc/1/cgroup | grep -qi docker && IsContainer=1 || IsContainer=0
+        cat /proc/1/cgroup | grep -qiE "docker|containerd" && IsContainer=1 || IsContainer=0
     fi
 
     if [ $IsContainer -eq 0 ];then
